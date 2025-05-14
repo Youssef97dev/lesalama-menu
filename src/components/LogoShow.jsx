6411adaebc1642b110455dafba4ecd1652e87476
Youssef97dev <youssefmoussafi97@gmail.com>
2025-05-14 10:25:53 +0100

    change design

--- src/components/LogoShow.jsx
+++ src/components/LogoShow.jsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 const LogoShow = () => {
   return (
-    <div className="px-2 py-1 border-[6px] border-[#d7b173] rounded-3xl bg-primary shadow-lg flex justify-center items-center">
+    <div className="p-3 border-[2px] border-[#d7b173] rounded-3xl bg-primary shadow-lg flex justify-center items-center">
       <Image
         src="/logo-salama-humberger-insta.png"
         alt="le salama marrakech"
         width={1000}
         height={1000}
-        className="w-24 lg:w-28"
+        className="w-20 lg:w-28"
       />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,28 @@
 import React from "react";
-import { FaWhatsappSquare, FaInstagramSquare } from "react-icons/fa";
+import { FaWhatsapp, FaInstagram } from "react-icons/fa";
 import Link from "next/link";
 import LogoShow from "./LogoShow";
 
 const Navbar = () => {
   return (
-    <div className="relative w-full flex justify-start items-center bg-[#d7b173] pb-4 shadow-lg z-50">
+    <div className="relative w-full flex justify-start items-center bg-[#d7b173] pb-1 shadow-lg z-50">
       <div className="w-full flex justify-start items-center bg-primary py-6 pl-5 gap-2">
         <Link
           href="http://wa.me/212675480018"
           target="_blank"
           className="text-[#d7b173]"
         >
-          <FaWhatsappSquare size={25} />
+          <FaWhatsapp size={25} />
         </Link>
         <Link
           href="https://www.instagram.com/lesalamamarrakech/"
           target="_blank"
           className="text-[#d7b173]"
         >
-          <FaInstagramSquare size={25} />
+          <FaInstagram size={25} />
         </Link>
       </div>
-      <div className="absolute top-5 w-full flex justify-center items-center">
+      <div className="absolute top-6 w-full flex justify-center items-center">
         <LogoShow />
       </div>
     </div>
